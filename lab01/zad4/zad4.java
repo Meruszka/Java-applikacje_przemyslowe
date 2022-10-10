4bae438f2b06f39c7c6c4b4f2cf3e2b90e2eeb3b
SMerski <merski70@gmail.com>
2022-10-10 09:08:23 +0200

    refactor zad4

--- lab01/zad4/zad4.java
+++ lab01/zad4/zad4.java
@@ -1,39 +1,45 @@
 package zad4;
 
 public class zad4 {
-    public static void main(String[] args){
-        System.out.println("Podaj liczbę: ");
-        int n = Integer.parseInt(System.console().readLine());
+    public static String drawTriangle(int n) {
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < n; i++){
             for(int j = i+1; j > 0; j--){
-                System.out.print("x");
+                sb.append("x");
             }
-            System.out.println();
+            sb.append("\n");
         }
         for(int i = n; i > 0; i--){
             for(int j = i; j > 0; j--){
-                System.out.print("x");
+                sb.append("x");
             }
-            System.out.println();
+            sb.append("\n");
         }
 
         for(int i=0; i<n; i++){
             for(int k=i; k>0; k--){
-                System.out.print(" ");
+                sb.append(" ");
             }
             for(int j=n-i; j>0; j--){
-                System.out.print("x");
+                sb.append("x");
             }
-            System.out.println();
+            sb.append("\n");
         }
         for(int i=n; i>0; i--){
             for(int k=i-1; k>0; k--){
-                System.out.print(" ");
+                sb.append(" ");
             }
             for(int j=n-i+1; j>0; j--){
-                System.out.print("x");
+                sb.append("x");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        return sb.toString();
+    }
+    public static void main(String[] args){
+        System.out.println("Podaj liczbę: ");
+        int n = Integer.parseInt(System.console().readLine());
+        System.out.println(drawTriangle(n));
+        
     }
 }
