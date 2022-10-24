b933eb9237b731a44cda9a897ff30ec7c3c0bc70
Meruszka <merski70@gmail.com>
2022-10-24 09:28:24 +0200

    lab03

---
+++ lab03/lab03/src/main/java/pl/smerski/app/lab03/Zad1Application.java
@@ -0,0 +1,24 @@
+package pl.smerski.app.lab03;
+
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.smerski.app.lab03.domain.Person;
+import pl.smerski.app.lab03.service.ParseCSV;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Zad1Application {
+	static ParseCSV parseCSV = new ParseCSV();
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		SpringApplication.run(Zad1Application.class, args);
+		String csv = parseCSV.getCSV();
+		for (CSVRecord record: parseCSV.parseCSV(csv)) {
+			Person person = new Person(record.get("id"), record.get("name"), record.get("surname"), record.get("email"), record.get("company_name"));
+			System.out.println(person);
+		}
+	}
+}
---
+++ lab03/lab03/src/main/java/pl/smerski/app/lab03/service/ParseCSV.java
@@ -0,0 +1,36 @@
+package pl.smerski.app.lab03.service;
+
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class ParseCSV {
+    String[] HEADERS = {"id", "name", "surname", "email", "company_name"};
+    public String getCSV() throws IOException, InterruptedException {
+        String URL = "https://stepik.org/media/attachments/lesson/266646/MOCK_DATA.csv";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+    public Iterable<CSVRecord> parseCSV(String csv) throws IOException {
+        Reader reader = new StringReader(csv);
+        return CSVFormat.DEFAULT
+                .withHeader(HEADERS)
+                .parse(reader);
+    }
+
+}
