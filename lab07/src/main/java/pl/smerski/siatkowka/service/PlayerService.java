8e0c32caa97ef9bb458eaaf91aafb291e22cbc34
Meruszka <merski70@gmail.com>
2022-12-19 08:07:19 +0100

    lab7

--- lab07/src/main/java/pl/smerski/siatkowka/service/PlayerService.java
+++ lab07/src/main/java/pl/smerski/siatkowka/service/PlayerService.java
@@ -24,16 +24,26 @@ public class PlayerService {
         Team team1 = new Team("PGE Skra", "Bełchatów", "Joel Banks", "Grzegorz Łomacz");
         Team team2 = new Team("Trefl", "Gdańsk", "Michał Winiarski", "Mariusz Wlazły");
 
-        teamRepository.save(team1);
-        teamRepository.save(team2);
 
         Player player1 = new Player("Jan", "Kowalski", 19, 190, team1);
         Player player2 = new Player("Olek", "Malinowski", 20, 200, team1);
         Player player3 = new Player("Golf", "Fiesta", 25, 150, team2);
-        
-        playerRepository.save(player1);
-        playerRepository.save(player2);
-        playerRepository.save(player3);
+
+        List<Player> t1 = new ArrayList<>();
+        t1.add(player1);
+        t1.add(player2);
+        List<Player> t2 = new ArrayList<>();
+        t2.add(player3);
+
+        team1.setPlayers(t1);
+        team2.setPlayers(t2);
+
+//        playerRepository.save(player1);
+//        playerRepository.save(player2);
+//        playerRepository.save(player3);
+
+        teamRepository.save(team1);
+        teamRepository.save(team2);
 
         Iterable<Player> players = playerRepository.findAll();
         System.out.println(players);
